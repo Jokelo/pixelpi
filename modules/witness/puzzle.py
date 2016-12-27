4e772c2c3c24cb99b943c3c808183b3a2b41e21f
Jokelo <yorusche@gmail.com>
2016-12-27 09:44:14 -0500

    Fixed 'random' not having attribute 'choice' error

--- modules/witness/puzzle.py
+++ modules/witness/puzzle.py
@@ -1,5 +1,5 @@
 from helpers import *
-import random
+import random as rdm
 from path import *
 
 from puzzlefeature import *
@@ -12,17 +12,17 @@ class Puzzle:
 		self.height = 5
 		self.cell_size = 3
 		
-		self.entry = Point(random.choice([0, self.width - 1]), random.choice([0, self.height - 1]))
+		self.entry = Point(rdm.choice([0, self.width - 1]), rdm.choice([0, self.height - 1]))
 
 		while True:			
-			self.exit = Point(random.choice([0, self.width - 1]), random.choice([0, self.height - 1]))
+			self.exit = Point(rdm.choice([0, self.width - 1]), rdm.choice([0, self.height - 1]))
 			if self.exit != self.entry:
 				break
 		
 		self.offset = Point(2 if self.entry.x == 0 else 1, 2 if self.entry.y == 0 else 1)
 		self.exit_on_screen = Point(self.offset.x + self.exit.x * self.cell_size, self.offset.y + self.exit.y * self.cell_size + (-1 if self.exit.y == 0 else 1))
 
-		self.hue = random.random()
+		self.hue = rdm.random()
 		self.background_color = hsv_to_color(self.hue, 0.9, 0.5)
 		self.path_color = hsv_to_color(self.hue, 0.35, 1.0)
 		self.line_color = hsv_to_color(self.hue, 1.0, 0.15)
@@ -45,15 +45,15 @@ class Puzzle:
 
 		zones = self.solution.get_zones()
 
-		total_features = random.randint(2, 8)
+		total_features = rdm.randint(2, 8)
 		if self.solution.zone_count == 1:
 			total_features += 5
 
 		for i in range(total_features):
-			feature_type = random.randint(0, 2)
+			feature_type = rdm.randint(0, 2)
 
 			if feature_type == 0 and len(available_mandatory_nodes) > 0:
-				mandatory_node = random.choice(available_mandatory_nodes)
+				mandatory_node = rdm.choice(available_mandatory_nodes)
 				available_mandatory_nodes.remove(mandatory_node)
 				self.features.append(MandatoryNode(self, mandatory_node))
 
@@ -62,8 +62,8 @@ class Puzzle:
 				node2 = None
 
 				while True:
-					node1 = Point(random.randint(0, self.width - 2), random.randint(0, self.height - 2))
-					if bool(random.getrandbits(1)):
+					node1 = Point(rdm.randint(0, self.width - 2), rdm.randint(0, self.height - 2))
+					if bool(rdm.getrandbits(1)):
 						node2 = Point(node1.x, node1.y + 1)
 					else:
 						node2 = Point(node1.x + 1, node1.y)
@@ -74,7 +74,7 @@ class Puzzle:
 				self.features.append(ForbiddenEdge(self, node1, node2))
 
 			if feature_type == 2 and len(available_mandatory_edges) > 1:
-				mandatory_edge = random.choice(available_mandatory_edges)
+				mandatory_edge = rdm.choice(available_mandatory_edges)
 				available_mandatory_edges.remove(mandatory_edge)
 				self.features.append(MandatoryEdge(self, mandatory_edge[0], mandatory_edge[1]))
 
@@ -88,12 +88,12 @@ class Puzzle:
 						if zones[x][y] == zone:
 							blocks.append(Point(x,y))
 
-				p = random.choice(blocks)
+				p = rdm.choice(blocks)
 				colorblocks.colors[p.x][p.y] = Color(0, 0, 0) if zones[p.x][p.y] % 2 == 0 else Color(255, 255, 255)
 
 
-			for i in range(random.randint(0, 8)):
-				p = Point(random.randint(0, self.width - 2), random.randint(0, self.height - 2))
+			for i in range(rdm.randint(0, 8)):
+				p = Point(rdm.randint(0, self.width - 2), rdm.randint(0, self.height - 2))
 				colorblocks.colors[p.x][p.y] = Color(0, 0, 0) if zones[p.x][p.y] % 2 == 0 else Color(255, 255, 255)
 			
 			self.features.append(colorblocks)
@@ -129,7 +129,7 @@ class Puzzle:
 
 		steps = [Point(dx / abs(dx), 0) for i in range(abs(dx))]
 		steps += [Point(0, dy / abs(dy)) for i in range(abs(dy))]
-		random.shuffle(steps)
+		rdm.shuffle(steps)
 
 		path = Path(self, screen)
 		path.start()
@@ -137,7 +137,7 @@ class Puzzle:
 			path.steps.append(Point(path.steps[len(path.steps) - 1].x + step.x, path.steps[len(path.steps) - 1].y + step.y))
 
 		# Randomly extend the path
-		for i in range(random.randint(4, 10)):
+		for i in range(rdm.randint(4, 10)):
 			path.extend()
 
 		return path
